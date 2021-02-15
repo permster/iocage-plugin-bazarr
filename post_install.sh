16a758187e26abbe55d5fbdc482388e327ea465a
Travis <tjgollih@gmail.com>
2021-02-14 20:10:45 -0700

    FIx commit overwrite issue.

--- post_install.sh
+++ post_install.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 
-# Clone from git repo
-git clone https://gitlab.com/LazyLibrarian/LazyLibrarian.git /usr/local/share/LazyLibrarian
+# Download latest v3 release
+fetch $(curl -s https://api.github.com/repos/morpheus65535/bazarr/releases/latest | grep tarball_url | cut -d '"' -f 4) -o /usr/local/share/tmp.tar.gz
+mkdir -p /usr/local/share/bazarr
+tar -xzvf /usr/local/share/tmp.tar.gz -C /usr/local/share/bazarr --strip-components=1
+rm /usr/local/share/tmp.tar.gz
+
+# install required pip packages
+yes | pip-3.7 install -r /usr/local/share/bazarr/requirements.txt
 
 # Create user
-pw user add lazylibrarian -c lazylibrarian -u 488 -d /nonexistent -s /usr/bin/nologin
+pw user add bazarr -c bazarr -u 1056 -d /nonexistent -s /usr/bin/nologin
 
-# make "lazylibrarian" the owner of the install location
+# make "bazarr" the owner of the install location
 mkdir /config
-chown -R lazylibrarian:lazylibrarian /usr/local/share/LazyLibrarian /config
+chown -R bazarr:bazarr /usr/local/share/bazarr /config
 
 #Set write permission to be able to write plugins update
-chmod 755 /usr/local/share/LazyLibrarian
+chmod 755 /usr/local/share/bazarr
 
 # Start the services
-chmod u+x /etc/rc.d/lazylibrarian
-sysrc -f /etc/rc.conf lazylibrarian_enable="YES"
-service lazylibrarian start
+chmod u+x /etc/rc.d/bazarr
+sysrc -f /etc/rc.conf bazarr_enable="YES"
+service bazarr start
 
-echo "LazyLibrarian successfully installed" > /root/PLUGIN_INFO
+echo "Bazarr successfully installed" > /root/PLUGIN_INFO
